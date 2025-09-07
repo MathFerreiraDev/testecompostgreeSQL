632c8eab1f373bac04a10e9ff82a7acf40e01bac
Matheus <mf8483226@gmail.com>
2025-09-06 21:30:57 -0300

    correcao server publico

--- app.js
+++ app.js
@@ -82,10 +82,11 @@ app.get('/events', (req, res) => {
 // serve static (build React)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// CORREÇÃO: usar '/*' (ou /.*/ ) em vez de '*' para evitar erro path-to-regexp
-app.get('/*', (req, res) => {
+// CORREÇÃO: usar expressão regular para rota curinga
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> console.log('Server listening on', PORT));
